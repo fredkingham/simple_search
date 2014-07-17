da602d3233074efe187b3e536e5de2fa3ec4a3af
Scott Raine <scott@potatolondon.com>
2014-07-17 16:12:46 +0100

    Allow the Index model to index arbitary items

--- simple_search/models.py
+++ simple_search/models.py
@@ -20,180 +20,6 @@ from django.conf import settings
 
 QUEUE_FOR_INDEXING = getattr(settings, "QUEUE_FOR_INDEXING", "default")
 
-def _do_index(instance, fields_to_index):
-    def get_data_from_field(field_, instance_):
-        lookups = field_.split("__")
-        value = instance
-        for lookup in lookups:
-            if value is None:
-                continue
-            value = getattr(value, lookup)
-
-            if "RelatedManager" in value.__class__.__name__:
-                if lookup == lookups[-2]:
-                    return [ getattr(x, lookups[-1]) for x in value.all() ]
-                else:
-                    raise TypeError("You can only index one level of related object")
-
-            elif hasattr(value, "__iter__"):
-                if lookup == lookups[-1]:
-                    return value
-                else:
-                    raise TypeError("You can only index one level of iterable")
-
-        return [ value ]
-
-    for field in fields_to_index:
-        texts = get_data_from_field(field, instance)
-        for text in texts:
-            if text is None:
-                continue
-
-            text = smart_unicode(text)
-            text = text.lower() #Normalize
-
-            words = text.split(" ") #Split on whitespace
-
-            #Build up combinations of adjacent words
-            for i in xrange(0, len(words)):
-                for j in xrange(1, 5):
-                    term_words = words[i:i+j]
-
-                    if len(term_words) != j:
-                        break
-
-                    term = u" ".join(term_words)
-
-                    if not term.strip(): continue
-
-                    @db.transactional(xg=True)
-                    def txn(term_):
-                        logging.info("Indexing: '%s', %s", term_, type(term_))
-                        term_count = text.count(term_)
-
-                        Index.objects.create(
-                            iexact=term_,
-                            instance_db_table=instance._meta.db_table,
-                            instance_pk=instance.pk,
-                            occurances=term_count
-                        )
-                        counter, created = GlobalOccuranceCount.objects.get_or_create(pk=term_)
-                        counter.count += term_count
-                        counter.save()
-
-                    while True:
-                        try:
-                            txn(term)
-                            break
-                        except db.TransactionFailedError:
-                            logging.warning("Transaction collision, retrying!")
-                            time.sleep(1)
-                            continue
-
-def _unindex_then_reindex(instance, fields_to_index):
-    unindex_instance(instance)
-    _do_index(instance, fields_to_index)
-
-def index_instance(instance, fields_to_index, defer_index=True):
-    """
-        If we are in a transaction, we must defer the unindex and reindex :(
-    """
-
-    if db.is_in_transaction() or defer_index:
-        defer(_unindex_then_reindex, instance, fields_to_index, _queue=QUEUE_FOR_INDEXING)
-    else:
-        unindex_instance(instance)
-        _do_index(instance, fields_to_index)
-
-def unindex_instance(instance):
-    indexes = Index.objects.filter(instance_db_table=instance._meta.db_table, instance_pk=instance.pk).all()
-    for index in indexes:
-
-        @db.transactional(xg=True)
-        def txn(_index):
-            count = GlobalOccuranceCount.objects.get(pk=_index.iexact)
-            count.count -= _index.occurances
-            count.save()
-            _index.delete()
-
-        try:
-            while True:
-                try:
-                    txn(index)
-                    break
-                except db.TransactionFailedError:
-                    logging.warning("Transaction collision, retrying!")
-                    time.sleep(1)
-                    continue
-        except GlobalOccuranceCount.DoesNotExist:
-            logging.warning("A GlobalOccuranceCount for Index: %s does not exist, ignoring", index.pk)
-            continue
-
-
-
-def parse_terms(search_string):
-    return shlex.split(smart_str(search_string.lower()))
-
-def search(model_class, search_string, per_page=50, current_page=1, total_pages=10, **filters):
-    terms = parse_terms(search_string)
-
-    #Get all matching terms
-    matching_terms = dict(GlobalOccuranceCount.objects.filter(pk__in=terms).values_list('pk', 'count'))
-    matches = Index.objects.filter(iexact__in=terms, instance_db_table=model_class._meta.db_table).all()
-
-    instance_weights = {}
-
-    for match in matches:
-        instance_weights.setdefault(match.instance_pk, []).append(matching_terms[match.iexact])
-
-    final_weights = []
-    for k, v in instance_weights.items():
-        """
-            This is where we rank the results. Lower scores are better. Scores are based
-            on the commonality of the word. More matches are rewarded, but not too much so
-            that rarer terms still have a chance.
-
-            Examples for n matches:
-
-            1 = 1 + (0 * 0.5) = 1    -> scores / 1
-            2 = 2 + (1 * 0.5) = 2.5  -> scores / 2.5 (rather than 2)
-            3 = 3 + (2 * 0.5) = 4    -> scores / 4 (rather than 3)
-        """
-
-        n = float(len(v))
-        final_weights.append((sum(v) / (n + ((n-1) * 0.5)), k))
-
-    final_weights.sort()
-
-    #Restrict to the max possible
-    final_weights = final_weights[:total_pages*per_page]
-
-    #Restrict to the page
-    offset = ((current_page - 1) * per_page)
-    final_weights = final_weights[offset:offset + per_page]
-
-    order = {}
-    for index, (score, pk) in enumerate(final_weights):
-        order[pk] = index
-
-
-
-    queryset = model_class.objects.all()
-    if filters:
-        queryset = queryset.filter(**filters)
-
-    # Workaround for an obscure bug when using datastore_utils.CachingQuerySet
-    # that returns no results when filtering by pk at this point.
-    # Feel free to investigate and fix it if you have any insight.
-    # results = queryset.filter(pk__in=order.keys())
-    results = [r for r in queryset if r.pk in order.keys()]
-    sorted_results = [None] * len(results)
-
-    for result in results:
-        position = order[result.pk]
-        sorted_results[position] = result
-
-    return sorted_results
 
 class GlobalOccuranceCount(models.Model):
     id = models.CharField(max_length=1024, primary_key=True)
@@ -216,28 +42,264 @@ class GlobalOccuranceCount(models.Model):
                 time.sleep(1)
                 continue
 
-class Index(models.Model):
+
+class AbstractIndex(models.Model):
     iexact = models.CharField(max_length=1024)
+    occurances = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+    def index(self, defer_index=True):
+        if db.is_in_transaction() or defer_index:
+            defer(self.reindex, _queue=QUEUE_FOR_INDEXING)
+        else:
+            self.reindex
+
+    def reindex(self):
+        self.unindex()
+        self._apply_index()
+
+    def unindex(self, instance):
+        indexes = self._get_indexes(instance)
+        for index in indexes:
+
+            @db.transactional(xg=True)
+            def txn(_index):
+                count = GlobalOccuranceCount.objects.get(pk=_index.iexact)
+                count.count -= _index.occurances
+                count.save()
+                _index.delete()
+
+            try:
+                while True:
+                    try:
+                        txn(index)
+                        break
+                    except db.TransactionFailedError:
+                        logging.warning("Transaction collision, retrying!")
+                        time.sleep(1)
+                        continue
+            except GlobalOccuranceCount.DoesNotExist:
+                logging.warning(
+                    "A GlobalOccuranceCount for Index: %s "
+                    "does not exist, ignoring", index.pk
+                )
+                continue
+
+    def search(self, model_class, search_string, per_page=50, current_page=1, total_pages=10, **filters):
+        terms = self.parse_terms(search_string)
+
+        instance_weights = self._get_matches(model_class, terms)
+
+        final_weights = []
+        for k, v in instance_weights.items():
+            """
+                This is where we rank the results. Lower scores are better. Scores are based
+                on the commonality of the word. More matches are rewarded, but not too much so
+                that rarer terms still have a chance.
+
+                Examples for n matches:
+
+                1 = 1 + (0 * 0.5) = 1    -> scores / 1
+                2 = 2 + (1 * 0.5) = 2.5  -> scores / 2.5 (rather than 2)
+                3 = 3 + (2 * 0.5) = 4    -> scores / 4 (rather than 3)
+            """
+
+            n = float(len(v))
+            final_weights.append((sum(v) / (n + ((n-1) * 0.5)), k))
+
+        final_weights.sort()
+
+        final_weights = final_weights[:total_pages*per_page]
+        #Restrict to the page
+        offset = ((current_page - 1) * per_page)
+        final_weights = final_weights[offset:offset + per_page]
+
+        order = {}
+        for index, (score, pk) in enumerate(final_weights):
+            order[pk] = index
+
+        queryset = model_class.objects.all()
+        if filters:
+            queryset = queryset.filter(**filters)
+
+        # Workaround for an obscure bug when using datastore_utils.CachingQuerySet
+        # that returns no results when filtering by pk at this point.
+        # Feel free to investigate and fix it if you have any insight.
+        # results = queryset.filter(pk__in=order.keys())
+        results = [r for r in queryset if r.pk in order.keys()]
+        sorted_results = [None] * len(results)
+
+        for result in results:
+            position = order[result.pk]
+            sorted_results[position] = result
+
+        return sorted_results
+
+    @classmethod
+    def parse_terms(cls, search_string):
+        return shlex.split(smart_str(search_string.lower()))
+
+    def _get_indexes(self, instance):
+        raise NotImplementedError("Subclasses should implement this.")
+
+    def _get_matches(self):
+        raise NotImplementedError("Subclasses should implement this.")
+
+    def _get_data(self, field, instance):
+        raise NotImplementedError("Subclasses should implement this.")
+
+    def _apply_index(self, instance, fields_to_index):
+        raise NotImplementedError("Subclasses should implement this.")
+
+    def _weight_results(self, instance_weights):
+        final_weights = []
+        for k, v in instance_weights.items():
+            """
+                This is where we rank the results. Lower scores are better. Scores are based
+                on the commonality of the word. More matches are rewarded, but not too much so
+                that rarer terms still have a chance.
+
+                Examples for n matches:
+
+                1 = 1 + (0 * 0.5) = 1    -> scores / 1
+                2 = 2 + (1 * 0.5) = 2.5  -> scores / 2.5 (rather than 2)
+                3 = 3 + (2 * 0.5) = 4    -> scores / 4 (rather than 3)
+            """
+
+            n = float(len(v))
+            final_weights.append((sum(v) / (n + ((n-1) * 0.5)), k))
+
+        return final_weights.sort()
+
+    def _apply_paging_to_results(self, final_weights, per_page, current_page, total_pages):
+        #Restrict to the max possible
+        final_weights = final_weights[:total_pages*per_page]
+
+        #Restrict to the page
+        offset = ((current_page - 1) * per_page)
+        return final_weights[offset:offset + per_page]
+
+
+class Index(AbstractIndex):
     instance_db_table = models.CharField(max_length=1024)
     instance_pk = models.PositiveIntegerField(default=0)
-    occurances = models.PositiveIntegerField(default=0)
 
     class Meta:
         unique_together = [
             ('iexact', 'instance_db_table', 'instance_pk')
         ]
 
+    def index(self, instance, fields_to_index, defer_index=True):
+        if db.is_in_transaction() or defer_index:
+            defer(self.reindex, instance, fields_to_index, _queue=QUEUE_FOR_INDEXING)
+        else:
+            self.reindex(instance, fields_to_index)
+
+    def reindex(self, instance, fields_to_index):
+        self.unindex(instance)
+        self._apply_index(instance, fields_to_index)
+
+    def _get_indexes(self, instance):
+        return Index.objects.filter(
+            instance_db_table=instance._meta.db_table, instance_pk=instance.pk).all()
+
+    def _get_data(self, field, instance):
+        lookups = field.split("__")
+        value = instance
+        for lookup in lookups:
+            if value is None:
+                continue
+            value = getattr(value, lookup)
+
+            if "RelatedManager" in value.__class__.__name__:
+                if lookup == lookups[-2]:
+                    return [getattr(x, lookups[-1]) for x in value.all()]
+                else:
+                    raise TypeError("You can only index one level of related object")
+
+            elif hasattr(value, "__iter__"):
+                if lookup == lookups[-1]:
+                    return value
+                else:
+                    raise TypeError("You can only index one level of iterable")
+
+        return [value]
+
+    def _get_matches(self, model_class, terms):
+        matching_terms = dict(GlobalOccuranceCount.objects.filter(pk__in=terms).values_list('pk', 'count'))
+        matches = Index.objects.filter(iexact__in=terms, instance_db_table=model_class._meta.db_table).all()
+
+        instance_weights = {}
+
+        for match in matches:
+            instance_weights.setdefault(match.instance_pk, []).append(matching_terms[match.iexact])
+
+        return instance_weights
+
+    def _apply_index(self, instance, fields_to_index):
+        for field in fields_to_index:
+            texts = self._get_data(field, instance)
+            for text in texts:
+                if text is None:
+                    continue
+
+                text = smart_unicode(text)
+                text = text.lower()  # Normalize
+
+                words = text.split(" ")  # Split on whitespace
+
+                #Build up combinations of adjacent words
+                for i in xrange(0, len(words)):
+                    for j in xrange(1, 5):
+                        term_words = words[i:i+j]
+
+                        if len(term_words) != j:
+                            break
+
+                        term = u" ".join(term_words)
+
+                        if not term.strip():
+                            continue
+
+                        @db.transactional(xg=True)
+                        def txn(term_):
+                            logging.info("Indexing: '%s', %s", term_, type(term_))
+                            term_count = text.count(term_)
+
+                            Index.objects.create(
+                                iexact=term_,
+                                instance_db_table=instance._meta.db_table,
+                                instance_pk=instance.pk,
+                                occurances=term_count
+                            )
+                            counter, created = GlobalOccuranceCount.objects.get_or_create(pk=term_)
+                            counter.count += term_count
+                            counter.save()
+
+                        while True:
+                            try:
+                                txn(term)
+                                break
+                            except db.TransactionFailedError:
+                                logging.warning("Transaction collision, retrying!")
+                                time.sleep(1)
+                                continue
+
 from django.dispatch import receiver
 from django.db.models.signals import post_save, pre_delete
+
 
 @receiver(post_save)
 def post_save_index(sender, instance, created, raw, *args, **kwargs):
     if getattr(instance, "Search", None):
         fields_to_index = getattr(instance.Search, "fields", [])
         if fields_to_index:
-            index_instance(instance, fields_to_index, defer_index=not raw) #Don't defer if we are loading from a fixture
+            instance.index(instance, fields_to_index, defer_index=not raw)  # Don't defer if we are loading from a fixture
+
 
 @receiver(pre_delete)
 def pre_delete_unindex(sender, instance, using, *args, **kwarg):
     if getattr(instance, "Search", None):
-        unindex_instance(instance)
+        instance.unindex()
--- simple_search/tests.py
+++ simple_search/tests.py
@@ -6,17 +6,12 @@ Replace this with more appropriate tests for your application.
 """
 
 import unittest
-from django.test import TestCase
 from django.db import models
+from django.test import TestCase
 from potatobase.testbase import PotatoTestCase
 
-from .models import (
-    GlobalOccuranceCount,
-    Index,
-    index_instance,
-    unindex_instance,
-    search
-)
+from .models import Index, GlobalOccuranceCount
+
 
 class SampleModel(models.Model):
     field1 = models.CharField(max_length=1024)
@@ -25,13 +20,14 @@ class SampleModel(models.Model):
     def __unicode__(self):
         return u"{} - {}".format(self.field1, self.field2)
 
+
 class SearchTests(PotatoTestCase):
     def test_field_indexing(self):
         instance1 = SampleModel.objects.create(
             field1="bananas apples cherries plums oranges kiwi"
         )
-
-        index_instance(instance1, ["field1"], defer_index=False)
+        i = Index()
+        i.index(instance1, ["field1"], defer_index=False)
 
         self.assertEqual(1, Index.objects.filter(iexact="bananas").count())
         self.assertEqual(1, Index.objects.filter(iexact="bananas apples").count())
@@ -54,19 +50,20 @@ class SearchTests(PotatoTestCase):
         instance2 = SampleModel.objects.create(field1="eat a chicken")
         instance3 = SampleModel.objects.create(field1="sleep a lot")
 
-        index_instance(instance1, ["field1"], defer_index=False)
-        index_instance(instance2, ["field1"], defer_index=False)
-        index_instance(instance3, ["field1"], defer_index=False)
+        i = Index()
+        i.index(instance1, ["field1"], defer_index=False)
+        i.index(instance2, ["field1"], defer_index=False)
+        i.index(instance3, ["field1"], defer_index=False)
 
-        results = search(SampleModel, "eat a")
+        results = i.search(SampleModel, "eat a")
 
         #Instance 3 should come last, because it only contains "a"
         self.assertEqual(instance3, results[2], results)
 
-        results = search(SampleModel, "eat fish")
+        results = i.search(SampleModel, "eat fish")
 
-        self.assertEqual(instance1, results[0]) #Instance 1 matches 2 uncommon words
-        self.assertEqual(instance2, results[1]) #Instance 2 matches 1 uncommon word
+        self.assertEqual(instance1, results[0])  # Instance 1 matches 2 uncommon words
+        self.assertEqual(instance2, results[1])  # Instance 2 matches 1 uncommon word
 
     def test_basic_searching(self):
         self.assertEqual(0, SampleModel.objects.count())
@@ -76,45 +73,48 @@ class SearchTests(PotatoTestCase):
         instance2 = SampleModel.objects.create(field1="banana", field2="Cherry")
         instance3 = SampleModel.objects.create(field1="BANANA")
 
-        index_instance(instance1, ["field1", "field2"], defer_index=False)
+        i = Index()
+
+        i.index(instance1, ["field1", "field2"], defer_index=False)
         self.assertEqual(2, Index.objects.count())
         self.assertEqual(1, GlobalOccuranceCount.objects.get(pk="banana").count)
         self.assertEqual(1, GlobalOccuranceCount.objects.get(pk="apple").count)
 
-        index_instance(instance2, ["field1", "field2"], defer_index=False)
+        i.index(instance2, ["field1", "field2"], defer_index=False)
 
         self.assertEqual(4, Index.objects.count())
         self.assertEqual(2, GlobalOccuranceCount.objects.get(pk="banana").count)
         self.assertEqual(1, GlobalOccuranceCount.objects.get(pk="apple").count)
         self.assertEqual(1, GlobalOccuranceCount.objects.get(pk="cherry").count)
 
-        index_instance(instance3, ["field1"], defer_index=False)
+        i.index(instance3, ["field1"], defer_index=False)
         self.assertEqual(5, Index.objects.count())
         self.assertEqual(3, GlobalOccuranceCount.objects.get(pk="banana").count)
         self.assertEqual(1, GlobalOccuranceCount.objects.get(pk="apple").count)
         self.assertEqual(1, GlobalOccuranceCount.objects.get(pk="cherry").count)
 
-        self.assertItemsEqual([instance1, instance2, instance3], search(SampleModel, "banana"))
-        self.assertItemsEqual([instance2], search(SampleModel, "cherry"))
+        self.assertItemsEqual([instance1, instance2, instance3], i.search(SampleModel, "banana"))
+        self.assertItemsEqual([instance2], i.search(SampleModel, "cherry"))
 
-        unindex_instance(instance1)
+        i.unindex(instance1)
 
-        self.assertItemsEqual([instance2, instance3], search(SampleModel, "banana"))
-        self.assertItemsEqual([instance2], search(SampleModel, "cherry"))
+        self.assertItemsEqual([instance2, instance3], i.search(SampleModel, "banana"))
+        self.assertItemsEqual([instance2], i.search(SampleModel, "cherry"))
 
     def test_additional_filters(self):
         instance1 = SampleModel.objects.create(field1="banana", field2="apple")
         instance2 = SampleModel.objects.create(field1="banana", field2="cherry")
         instance3 = SampleModel.objects.create(field1="pineapple", field2="apple")
 
-        index_instance(instance1, ["field2"], defer_index=False)
-        index_instance(instance2, ["field2"], defer_index=False)
-        index_instance(instance3, ["field2"], defer_index=False)
+        i = Index()
+        i.index(instance1, ["field2"], defer_index=False)
+        i.index(instance2, ["field2"], defer_index=False)
+        i.index(instance3, ["field2"], defer_index=False)
 
-        self.assertItemsEqual([instance1, instance3], search(SampleModel, "apple"))
+        self.assertItemsEqual([instance1, instance3], i.search(SampleModel, "apple"))
 
         # Now pass to search a queryset filter and check that it's applied
-        self.assertItemsEqual([instance1], search(SampleModel, "apple", **{'field1': 'banana'}))
+        self.assertItemsEqual([instance1], i.search(SampleModel, "apple", **{'field1': 'banana'}))
 
     @unittest.skip("Not implemented yet")
     def test_logic_searching(self):
@@ -122,14 +122,15 @@ class SearchTests(PotatoTestCase):
         instance2 = SampleModel.objects.create(field1="banana", field2="Cherry")
         instance3 = SampleModel.objects.create(field1="BANANA")
 
-        index_instance(instance1, ["field1", "field2"], defer_index=False)
-        index_instance(instance2, ["field1", "field2"], defer_index=False)
-        index_instance(instance3, ["field1"], defer_index=False)
+        i = Index()
+        i.index(instance1, ["field1", "field2"], defer_index=False)
+        i.index(instance2, ["field1", "field2"], defer_index=False)
+        i.index(instance3, ["field1"], defer_index=False)
 
-        self.assertItemsEqual([instance1], search(SampleModel, "banana AND apple"))
-        self.assertItemsEqual([instance1, instance2], search(SampleModel, "apple OR cherry"))
+        self.assertItemsEqual([instance1], i.search(SampleModel, "banana AND apple"))
+        self.assertItemsEqual([instance1, instance2], i.search(SampleModel, "apple OR cherry"))
 
-        unindex_instance(instance1)
+        i.unindex(instance1)
 
-        self.assertItemsEqual([], search(SampleModel, "banana AND apple"))
-        self.assertItemsEqual([instance2], search(SampleModel, "apple OR cherry"))
+        self.assertItemsEqual([], i.search(SampleModel, "banana AND apple"))
+        self.assertItemsEqual([instance2], i.search(SampleModel, "apple OR cherry"))
